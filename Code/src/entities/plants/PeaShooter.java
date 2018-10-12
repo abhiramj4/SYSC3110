e63a2c57d69a21b8238462b1e080b7d4d6c9da9b
Sai Vikranth Desu <vikranthdesu@gmail.com>
2018-10-12 16:27:28 -0400

    more merge fix pls

--- Code/src/entities/plants/PeaShooter.java
+++ Code/src/entities/plants/PeaShooter.java
@@ -1,24 +1,27 @@
 package entities.plants;
 
-public class PeaShooter extends Plant{
+import entities.plants.Plant;
 
-	PeaShooter(String name, int damage, int coolDown, int cost, int health) {
-		super(name, damage, coolDown, cost, health);
+public class PeaShooter extends Plant{
+	
+	private final static String NAME = "peaShooter";
+	private final static int DAMAGE = 1;
+	private final static int COOLDOWN = 3;
+	private final static int COST = 10;
+	private final static int HEALTH = 3;
+	
+	PeaShooter(String name, Integer damage, Integer coolDown, Integer cost, Integer health) {
+		super(NAME, DAMAGE, COOLDOWN, COST, HEALTH);
 		// TODO Auto-generated constructor stub
-		
-		this.setDamage(10); //change this as you see fit
+	}
+	
+	public String toString() {
+		return (this.getName() + "pea shooter");
 	}
 
 	@Override
 	public void setDamage(int damage) {
 		// TODO Auto-generated method stub
-		//basic damage algorithm
-		this.damage = damage;
+		//for more complex damage algorithms
 	}
-	
-	public String toString() {
-		
-		return (this.getName() + "pea shooter");
-	}
-
 }
