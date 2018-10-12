4285925c1436a850f9f9ff1dc390211f5e765cca
Abhi Santhosh <abhisanthosh@cmail.carleton.ca>
2018-10-12 15:23:26 -0400

    Add files via upload
    Plant, sunflower and peashooter done

---
+++ Code/src/entities/plants/PeaShooter.java
@@ -0,0 +1,21 @@
+
+public class PeaShooter extends Plant{
+
+	PeaShooter(String name, Integer damage, Integer coolDown, Integer cost, Integer health) {
+		super(name, damage, coolDown, cost, health);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setDamage(Integer damage) {
+		// TODO Auto-generated method stub
+		//basic damage algorithm
+		this.damage = damage;
+	}
+	
+	public String toString() {
+		
+		return (this.getName() + "pea shooter");
+	}
+
+}
---
+++ Code/src/entities/plants/Sunflower.java
@@ -0,0 +1,25 @@
+
+public class Sunflower extends Plant{
+
+	private Integer sunPerTurn;
+	
+	Sunflower(String name, Integer damage, Integer coolDown, Integer cost, Integer sun, Integer health) {
+		super(name, damage, coolDown, cost, health);
+		// TODO Auto-generated constructor stub
+		this.sunPerTurn = sun;
+	}
+
+	@Override
+	public void setDamage(Integer damage) {
+		// TODO Auto-generated method stub
+		this.damage = 0;
+		
+	}
+	
+	public String toString() {
+		
+		return (this.getName() + "Sun flower");
+	}
+	
+	
+}
