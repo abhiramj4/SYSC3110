6ba189fa2cc7d97459b248efdd6b1eacd61d542d
Abhi Santhosh <abhisanthosh@cmail.carleton.ca>
2018-10-12 16:04:05 -0400

    Added constants

--- Code/src/entities/plants/Sunflower.java
+++ Code/src/entities/plants/Sunflower.java
@@ -2,12 +2,19 @@
 public class Sunflower extends Plant{
 
 	private Integer sunPerTurn;
+	private final static String NAME = "SunFlower";
+	private final static int DAMAGE = 0;
+	private final static int COOLDOWN = 3;
+	private final static int COST = 10;
+	private final static int SUN = 10;
+	private final static int HEALTH = 3;
 	
-	Sunflower(String name, Integer damage, Integer coolDown, Integer cost, Integer sun, Integer health) {
-		super(name, damage, coolDown, cost, health);
+	Sunflower(Coordinate coordinate) {
+		super(NAME, DAMAGE, COOLDOWN, COST, HEALTH);
 		// TODO Auto-generated constructor stub
-		this.sunPerTurn = sun;
+		this.sunPerTurn = SUN;
 	}
+	
 
 	@Override
 	public void setDamage(Integer damage) {
