781f75eefba16f713390cd7a91ca4e1db818d3b5
Sai Vikranth Desu <vikranthdesu@gmail.com>
2018-10-29 13:43:53 -0400

    trynna pull

--- Code/src/entities/zombies/BaseZombie.java
+++ Code/src/entities/zombies/BaseZombie.java
@@ -29,18 +29,13 @@ public class BaseZombie extends Zombie {
 			Coordinate toCheck = new Coordinate(curr.getX() -1, curr.getY());
 			
 			if (g.getGameboard().getSquare(toCheck).isEmpty()) {
-				if (g.getGameboard().getSquare(curr).getCoordinate().getX() == 0) {
-					g.GameOver();
-				} else {
-					g.getGameboard().move(curr, new Coordinate(curr.getX() - 1, curr.getY()));
-				}
+				g.getGameboard().move(curr, new Coordinate(curr.getX() - 1, curr.getY()));
 			} else if (g.getGameboard().getSquare(toCheck).getEntity().getEntityType() == EntityType.PLANT) {
 				int orighealth = g.getGameboard().getSquare(toCheck).getEntity().getHealth();
-				int damage = ((Zombie) g.getGameboard().getSquare(curr).getEntity()).getDamage();
-				if ((orighealth - damage) < 0) {
+				if ((orighealth - getDamage()) < 0) {
 					g.getGameboard().removeEntity(g, curr);
 				} else {
-					g.getGameboard().getSquare(toCheck).getEntity().setHealth(orighealth - damage);
+					g.getGameboard().getSquare(toCheck).getEntity().setHealth(orighealth - getDamage());
 				}
 			}
 		}
