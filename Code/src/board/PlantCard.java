cf58fb05effe724384fd1b828352f24ad7e41056
Abhi <abhisanthosh@cmail.carleton.ca>
2018-11-25 12:55:43 -0500

    Update PlantCard.java
    Added paths for the cards, images aren't working

--- Code/src/board/PlantCard.java
+++ Code/src/board/PlantCard.java
@@ -1,9 +1,12 @@
 package board;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 
 public class PlantCard extends Button {
 
+	private final static String SUNIMAGEPATH = "resources/images/plants/sunflower.png";
+	private final static String PEAIMAGEPATH = "resources/images/plants/peashooter.png";
 	private String plantname;
 	private int cost;
 
@@ -12,6 +15,7 @@ public class PlantCard extends Button {
 		this.plantname = plantname;
 		this.cost = cost;
 		this.setText(plantname + "\n" + cost);
+
 	}
 
 	/**
@@ -27,6 +31,7 @@ public class PlantCard extends Button {
 	public void setPlantname(String plantname) {
 		this.plantname = plantname;
 	}
+	
 
 	/**
 	 * @return the cost
